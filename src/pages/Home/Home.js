bcede0f95b8cc9da69c12a0d490555b4d7aeaca3
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2023-07-21 08:50:51 +0700

    footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,10 @@ import classnames from 'classnames/bind';
 import styles from './Home.module.scss';
 
 const cx = classnames.bind(styles);
+// Hi, Monica
 
 function Home() {
-    return <div className={cx('wrapper')}>Home</div>;
+    return <div className={cx('wrapper')}>home</div>;
 }
 
 export default Home;
