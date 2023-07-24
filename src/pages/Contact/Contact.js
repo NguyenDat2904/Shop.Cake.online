750599619e66c5778ee9e367936f15ab280c38a5
nmk15112000 <mauquipro1@gmail.com>
2023-07-24 21:10:20 +0700

    Push 2

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import "./Contact.css";
+import { Form } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMobile } from '@fortawesome/free-solid-svg-icons';
 function Contact() {
     return <div className="contact">
     <div className="Hop">
@@ -8,17 +13,19 @@ function Contact() {
                 <h3>Thông tin liên hệ</h3>
                 <hr className="line"></hr>
                 <p>Với mục tiêu trở thành điểm lựa chọn tin cậy, duy nhất và tốt nhất về dòng bánh kem cao cấp, chúng tôi cam kết mang đến cho khách hàng những dịch vụ gia tăng hoàn hảo với thái dộ phục vụ tốt nhất theo đúng thông điệp mà chúng tôi muốn gửi đến khách hàng.</p>
-            <p>344 Huỳnh Tấn Phát, Phường Bình Thuận, Quận 7, Tp. Hồ Chí Minh</p>
-            <p>1900 9477</p>
-            <p>admin@demo037126.web30s.vn</p>
+            <p className="contact-body"> <FontAwesomeIcon icon={faLocationDot} classname="icon"/>  344 Huỳnh Tấn Phát, Phường Bình Thuận, Quận 7, Tp. Hồ Chí Minh</p>
+            <p  className="contact-body"> <FontAwesomeIcon icon={faEnvelope} classname="icon"/>  1900 9477</p>
+             <p  className="contact-body"> <FontAwesomeIcon icon={faMobile} classname="icon"/>  admin@demo037126.web30s.vn</p>
             </div>
             <div class="contact-right">
-                <input type="text" placeholder="Họ và tên"></input>
-                <input type="text" placeholder="Email"></input>
-                <input type="text" placeholder="Số điện thoại"></input>
+                <form>
+                <input type="text" placeholder="Họ và tên"></input><br/>
+                <input className="required"  type="text" placeholder="Email"></input><br/>
+                <input className="required" type="text" placeholder="Số điện thoại"></input><br/>
                 <textarea placeholder="Nội dung"></textarea>
-                <input type="text" placeholder="Mã bảo mật"></input>
-                <input className="button" type="button" Value="Gửi liên hệ"></input>
+                <input type="text" placeholder="Mã bảo mật"></input><br/>
+                <input className="button" type="button" Value="Gửi liên hệ"></input><br/>
+                </form>
             </div>
         </div>
         </div>
