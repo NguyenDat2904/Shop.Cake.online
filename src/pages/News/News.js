f1523d0445e4ba8e3c2400f44d39d92e016d28f8
nmk15112000 <mauquipro1@gmail.com>
2023-07-24 22:07:23 +0700

    Push 3

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import Item1 from './Item/Item1.js';
 function News() {
-    return <div>News</div>;
+    return <div>
+<Item1/>
+    </div>;
 }
 
 export default News;
