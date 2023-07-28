09f0dadd3d477ccda382291363af00fa0e7dec4e
nmk15112000 <mauquipro1@gmail.com>
2023-07-28 21:56:27 +0700

    Push 4

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,9 +1,56 @@
 import React from 'react';
-import Item1 from './Item/Item1.js';
+import styles from './News.module.scss';
+import classnames from 'classnames/bind';
+import SideBar from '../Product/SideBar/SideBar';
+import Select from '~/component/Select/Select';
+import Banner from '';
+const cx = classnames.bind(styles);
 function News() {
-    return <div>
-<Item1/>
-    </div>;
+    return (
+        <div>
+            <Banner page="Tin tức" title="Tin tức" />
+            <div className={cx('News')}>
+                <div className={cx('Hop')}>
+                    <div className={cx('news-left')}>
+                        <div className="Sidebar_1">
+                            <SideBar title="Nhập từ khoá"></SideBar>
+                        </div>
+                        <div className="Sidebar_2">
+                            {' '}
+                            <SideBar title="Danh mục Tin tức">
+                                <Select title="Mẫu Bánh mới" />
+                                <Select title="Tin vắn" />
+                            </SideBar>
+                        </div>
+                        <div className="Sidebar_3">
+                            {' '}
+                            <SideBar title="BÀI VIẾT MỚI NHẤT"></SideBar>
+                        </div>
+                        <div className="Sidebar_4">
+                            <SideBar title="TAG"></SideBar>
+                        </div>
+                    </div>
+                    <div className={cx('news-right')}>
+                        <form>
+                            <img className={cx('New_img')}></img>
+                            <p className="New_text">
+                                Biết bạn thân thích "đơn giản không màu mè", cả nhóm hùa nhau tặng cái bánh kem làm khổ
+                                chủ mở ra "xỉu liền tại chỗ"
+                            </p>
+                        </form>
+                        <form>
+                            <img className={cx('New_img2')}></img>
+                            <p className="New_text">Độc đáo bánh kem điêu khắc của cô gái 9X quê Cà Mau</p>
+                        </form>
+                        <form>
+                            <img className={cx('New_img3')}></img>
+                            <p className="New_text">Chiếc bánh sinh nhật hot nhất hiện nay: Mở ra biết ngay tình bạn</p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default News;
