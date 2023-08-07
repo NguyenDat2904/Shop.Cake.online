35679f3b7c6536e49e57738906ae43e7305a39f8
nmk15112000 <mauquipro1@gmail.com>
2023-08-07 12:20:01 +0700

    Push 7

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -66,30 +66,30 @@ function News() {
                             </SideBar>
                     </div>
                     <div className={cx('news-right')}>
-                        <form>
+                        <form className={cx('news-right-div')}>
                             <img className={cx('New_img')}></img>
                             <a className={cx('new_text')} href="https://demo037126.web30s.vn/biet-ban-than-thich-don-gian-khong-mau-me-ca-nhom-hua-nhau-tang-cai-banh-kem-lam-kho-chu-mo-ra-xiu-lien-tai-cho" > 
                                 Biết bạn thân thích "đơn giản không màu mè", cả nhóm hùa nhau tặng cái bánh kem làm khổ
                                 chủ mở ra "xỉu liền tại chỗ"
                             </a>
                         </form>
-                        <form>
+                        <form className={cx('news-right-div')}>
                             <img className={cx('New_img2')}></img>
                             <a className={cx('new_text')}>Độc đáo bánh kem điêu khắc của cô gái 9X quê Cà Mau</a>
                         </form>
-                        <form>
+                        <form className={cx('news-right-div')}>
                             <img className={cx('New_img3')}></img>
                             <a className={cx('new_text')}>Chiếc bánh sinh nhật hot nhất hiện nay: Mở ra biết ngay tình bạn</a>
                         </form>
-                        <form>
+                        <form className={cx('news-right-div')}>
                             <img className={cx('New_img4')}></img>
                             <a className={cx('new_text')}>Bị người thân úp mặt vào bánh sinh nhật, cậu bé có hành động khiến tất cả sững sờ, dân mạng lại nhiệt tình ủng hộ</a>
                         </form>
-                        <form>
+                        <form className={cx('news-right-div')}>
                             <img className={cx('New_img5')}></img>
                             <a className={cx('new_text')}>Ngã ngửa khi mua bánh sinh nhật online</a>
                             </form>
-                                <form>
+                            <form className={cx('news-right-div')}>
                             <img className={cx('New_img6')}></img>
                             <a className={cx('new_text')}>Chiếc bánh sinh nhật của Gen Z "điềm tĩnh và không thích màu mè" gây bất ngờ vì cách trang trí, nhận được lời khen không ngớt từ dân mạng</a>
                         </form>
